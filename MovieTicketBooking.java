23c026c321157df4a3153506db1492846b3307c2
vasanthkrishnan <vasanthakrishnans22it@srishakthi.ac.in>
2024-11-21 22:21:48 +0530

    Intial commit

---
+++ MovieTicketBooking.java
@@ -0,0 +1,44 @@
+class User
+{
+    String name, phone, email, address, username, password;
+
+    public void BuyTicket()
+    {
+
+    }
+
+    public void ShowTicket()
+    {
+
+    }
+
+    public void CancleTicket()
+    {
+
+    }
+
+    public void LogOut()
+    {
+
+    }
+}
+
+class Admin extends User
+{
+    public void AddMovies()
+    {
+
+    }
+
+    public void SelectMovies()
+    {
+
+    }
+}
+
+public class MovieTicketBooking
+{
+    public static void main(String[] args) {
+        
+    }
+}
